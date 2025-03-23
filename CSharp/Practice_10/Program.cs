084e9d6900f7113da9e166a637c14cd7756bf9ee
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:33:35 +0300

    Practice 10 attempt 1

---
+++ CSharp/Practice_10/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice_10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random randomNumberGenerator = new Random();
+        int number = randomNumberGenerator.Next(0, 101);
+        int sum = 0;
+ 
+        for (int i = 0; i <= number; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0)
+                sum += i;
+        }
+        
+        Console.WriteLine($"Случайное число: {number}");
+        Console.WriteLine($"Сумма положительных чисел, кратных 3 или 5: {sum}");
+    }
+}
