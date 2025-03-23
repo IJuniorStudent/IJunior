c0d9af0c546a4d61694a521c26b53d2f3e799eb4
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:11:05 +0300

    Practice 3 attempt 1

---
+++ CSharp/Practice_3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Practice_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int totalPicturesInAlbum = 52;
+        int picturesPerLine = 3;
+        
+        int albumFullLinesCount = totalPicturesInAlbum / picturesPerLine;
+        int albumLastLinePicturesCount = totalPicturesInAlbum % picturesPerLine;
+        
+        Console.WriteLine($"В альбоме всего картинок: {totalPicturesInAlbum}");
+        Console.WriteLine($"Количество заполненных рядов с картинками: {albumFullLinesCount}");
+        Console.WriteLine($"Количество картинок, занимающих неполный ряд: {albumLastLinePicturesCount}");
+    }
+}
