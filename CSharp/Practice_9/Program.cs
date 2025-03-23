e77ecd19e5bbd464e7a1e38114e7a241fccd105a
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:32:24 +0300

    Practice 9 attempt 1

---
+++ CSharp/Practice_9/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice_9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sequenceStartNumber = 5;
+        int sequenceMaxNumber = 103;
+        int sequenceStepIncrement = 7;
+ 
+        for (int i = sequenceStartNumber; i <= sequenceMaxNumber; i += sequenceStepIncrement)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
