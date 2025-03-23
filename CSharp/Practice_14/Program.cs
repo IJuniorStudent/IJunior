e2cccbce445affb73d5e82223acdfa88247beeb5
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:44:35 +0300

    Practice 14 attempt 2

--- CSharp/Practice_14/Program.cs
+++ CSharp/Practice_14/Program.cs
@@ -5,18 +5,19 @@ class Program
     static void Main(string[] args)
     {
         string unlockPassword = "secrets";
-        int inputPasswordAttemptsRemain = 3;
- 
-        while (--inputPasswordAttemptsRemain >= 0)
+        int inputPasswordAttemptsMaxCount = 3;
+        
+        for (int i = 0; i < inputPasswordAttemptsMaxCount; i++)
         {
             Console.Write("Введите пароль для разблокировки: ");
+            
             if (Console.ReadLine() == unlockPassword)
+            {
+                Console.WriteLine("Секретики!");
                 break;
+            }
             
             Console.WriteLine("Неверный пароль");
         }
- 
-        if (inputPasswordAttemptsRemain >= 0)
-            Console.WriteLine("Секретики!");
     }
 }
