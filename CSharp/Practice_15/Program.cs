0b5af4bf38c80f4c2ca59badeabda2f21ecd06ac
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:46:11 +0300

    Practice 15 attempt 1

---
+++ CSharp/Practice_15/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Practice_15;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int DivisorMinValue = 10;
+        const int DivisorMaxValue = 25;
+        
+        const int FullDivideSearchRangeMin = 50;
+        const int FullDivideSearchRangeMax = 150;
+        
+        Random random = new Random();
+        int divisor = random.Next(DivisorMinValue, DivisorMaxValue + 1);
+        
+        Console.WriteLine($"Выбранное число в диапазоне от {DivisorMinValue} до {DivisorMaxValue}: {divisor}");
+        Console.WriteLine();
+        
+        int rangeRemainder = FullDivideSearchRangeMin;
+        while (rangeRemainder > 0)
+            rangeRemainder -= divisor;
+        
+        int fullDivisorsCount = 0;
+        
+        for (int i = FullDivideSearchRangeMin - rangeRemainder; i <= FullDivideSearchRangeMax; i += divisor)
+        {
+            fullDivisorsCount++;
+            Console.WriteLine($"Делитель {fullDivisorsCount}: {i}");
+        }
+        
+        Console.WriteLine();
+        Console.WriteLine($"Количество делителей без остатка на {divisor} в диапазоне от {FullDivideSearchRangeMin} до {FullDivideSearchRangeMax}: {fullDivisorsCount}");
+    }
+}
