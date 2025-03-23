7b0f228f70d5ac94d933400d61894200a2d86f83
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:14:49 +0300

    Practice 6 attempt 1

---
+++ CSharp/Practice_6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice_6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int minutesPerHour = 60;
+        int patientWaitTimeMinutes = 10;
+        
+        Console.Write("Введите количество пациентов: ");
+        int patientsCount = Convert.ToInt32(Console.ReadLine());
+        
+        int totalWaitTime = patientsCount * patientWaitTimeMinutes;
+        int waitTimeHours = totalWaitTime / minutesPerHour;
+        int waitTimeMinutes = totalWaitTime % minutesPerHour;
+        
+        Console.WriteLine($"Вы должны отстоять в очереди {waitTimeHours} часа и {waitTimeMinutes} минут.");
+        Console.ReadLine();
+    }
+}
