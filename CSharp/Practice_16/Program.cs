4f29510bc494849f84fc9f48f253e2709d11331e
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:47:23 +0300

    Practice 16 attempt 1

---
+++ CSharp/Practice_16/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Practice_16;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int randomNumberRangeMin = 0;
+        int randomNumberRangeMax = 100000;
+        int baseValue = 2;
+        
+        Random random = new Random();
+        int randomNumber = random.Next(randomNumberRangeMin, randomNumberRangeMax + 1);
+        
+        int powerOfBase = 1;
+        int searchValue = baseValue;
+        
+        while (searchValue <= randomNumber)
+        {
+            searchValue *= baseValue;
+            powerOfBase++;
+        }
+        
+        Console.WriteLine($"Случайно выбранное значение: {randomNumber}");
+        Console.WriteLine($"Значение, превосходящее выбранное: {searchValue}");
+        Console.WriteLine($"Степень числа {baseValue}: {powerOfBase}");
+    }
+}
