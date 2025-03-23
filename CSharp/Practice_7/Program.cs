c7c6b87cef62df36279896a2b6f851bc156c272c
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:28:42 +0300

    Practice 7 attempt 1

---
+++ CSharp/Practice_7/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice_7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите сообщение: ");
+        string displayMessage = Console.ReadLine();
+        
+        Console.Write("Введите количество повторов сообщения: ");
+        int repeatCount = Convert.ToInt32(Console.ReadLine());
+        
+        while (repeatCount-- > 0)
+        {
+            Console.WriteLine($"Сообщение: {displayMessage}");
+        }
+        
+        Console.ReadKey();
+    }
+}
