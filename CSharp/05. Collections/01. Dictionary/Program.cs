bef0bec3c1707f0c164906815b7d83406ea44112
IJuniorStudent <jolan07@list.ru>
2025-03-24 02:25:37 +0300

    Practice 32 attempt 1

---
+++ CSharp/05. Collections/01. Dictionary/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Practice_32;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string CommandExit = "exit";
+        
+        Dictionary<string, string> wordExplainStorage = new Dictionary<string, string>
+        {
+            ["Функция"] = "Блок кода для повторного использования без дублирования",
+            ["Метод"] = "Блок кода для повторного использования без дублирования, который является членом класса"
+        };
+        
+        bool isAppRun = true;
+        
+        while (isAppRun)
+        {
+            Console.Clear();
+            
+            Console.WriteLine($"Напишите слово для получения его значения или \"{CommandExit}\" для выхода.");
+            Console.WriteLine("Доступные слова:");
+            
+            foreach (var word in wordExplainStorage.Keys)
+                Console.WriteLine($"- {word}");
+            
+            Console.Write("> ");
+            
+            string userInput = Console.ReadLine();
+            
+            switch (userInput)
+            {
+                case CommandExit:
+                    isAppRun = false;
+                    Console.WriteLine("До свидания!");
+                    break;
+                
+                default:
+                    Console.WriteLine();
+                    
+                    if (wordExplainStorage.ContainsKey(userInput))
+                        Console.WriteLine($"Значение слова \"{userInput}\": {wordExplainStorage[userInput]}");
+                    else
+                        Console.WriteLine($"Слово \"{userInput}\" не найдено");
+                    
+                    Console.WriteLine();
+                    Console.WriteLine("Для продолжения нажмите любую клавишу...");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
