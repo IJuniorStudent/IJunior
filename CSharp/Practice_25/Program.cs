479753192223a27d0d51fbf4e3a77c98926b71c2
IJuniorStudent <jolan07@list.ru>
2025-03-24 01:03:18 +0300

    Practice 25 attempt 1

---
+++ CSharp/Practice_25/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Practice_25;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int arrayLength = 20;
+        int[] numbers = new int[arrayLength];
+        
+        Console.WriteLine("Массив с исходными данными:");
+        
+        for (int i = 0; i < arrayLength; i++)
+        {
+            numbers[i] = i + 1;
+            Console.Write($"{numbers[i]} ");
+        }
+        
+        Console.WriteLine("\n");
+        Console.Write("Введите количество сдвигов массива влево: ");
+        
+        int shiftCount = Convert.ToInt32(Console.ReadLine());
+        
+        for (int i = 0; i < shiftCount; i++)
+        {
+            for (int j = 0; j < arrayLength - 1; j++)
+                (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
+        }
+        
+        Console.WriteLine();
+        Console.WriteLine("Массив со смещенными данными:");
+        
+        for (int i = 0; i < arrayLength; i++)
+            Console.Write($"{numbers[i]} ");
+        
+        Console.WriteLine();
+    }
+}
