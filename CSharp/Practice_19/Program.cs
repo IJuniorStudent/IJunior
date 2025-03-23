0c2cce794283b8c327cdc95cd487f0f67909e2a0
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:52:07 +0300

    Practice 19 attempt 1

---
+++ CSharp/Practice_19/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Practice_19;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int matrixRowCount = 10;
+        int matrixColumnCount = 10;
+        
+        int randValueMin = 1;
+        int randValueMax = 9;
+        
+        Random random = new Random();
+        int[,] matrix = new int[matrixRowCount, matrixColumnCount];
+        int matrixMaxValue = int.MinValue;
+        
+        Console.WriteLine("Исходная матрица со случайными значениями:");
+        
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                int randomValue = random.Next(randValueMin, randValueMax + 1);
+                if (randomValue > matrixMaxValue)
+                    matrixMaxValue = randomValue;
+                
+                matrix[i, j] = randomValue;
+                Console.Write($"{randomValue} ");
+            }
+            
+            Console.WriteLine();
+        }
+        
+        Console.WriteLine();
+        Console.WriteLine($"Наибольшее число в матрице: {matrixMaxValue}");
+        
+        Console.WriteLine();
+        Console.WriteLine("Измененная матрица:");
+        
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i, j] == matrixMaxValue)
+                    matrix[i, j] = 0;
+                
+                Console.Write($"{matrix[i, j]} ");
+            }
+            
+            Console.WriteLine();
+        }
+    }
+}
