12d9f093d8c0077981b57bceaa25cb799d589367
IJuniorStudent <jolan07@list.ru>
2025-03-24 01:06:02 +0300

    Practice 27 attempt 1

---
+++ CSharp/Practice_27/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice_27;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число для преобразования в целочисленный тип");
+        
+        int userNumber = ReadInputNumber();
+        
+        Console.WriteLine($"Поздравляем! Вы успешно ввели число: {userNumber}");
+    }
+ 
+    static int ReadInputNumber()
+    {
+        while (true)
+        {
+            Console.Write("> ");
+            
+            if (int.TryParse(Console.ReadLine(), out int number))
+                return number;
+            
+            Console.WriteLine("Не удалось преобразовать ввод в число, попробуйте еще раз");
+        }
+    }
+}
