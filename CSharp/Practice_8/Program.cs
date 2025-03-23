5ef230bbda4e1cd312025052c0f0730052893896
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:31:23 +0300

    Practice 8 attempt 1

---
+++ CSharp/Practice_8/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Practice_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string commandExit = "exit";
+        string loopMessage = $"Программа выполняется. Для выхода введите \"{commandExit}\"\n> ";
+        
+        Console.Write(loopMessage);
+        
+        while (Console.ReadLine() != commandExit)
+        {
+            Console.Write(loopMessage);
+        }
+        
+        Console.WriteLine("Программа завершена");
+    }
+}
