9195581c3531158eda8126ab95fceb4b5da27452
IJuniorStudent <jolan07@list.ru>
2025-03-24 01:04:58 +0300

    Practice 26 attempt 1

---
+++ CSharp/Practice_26/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Practice_26;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string expression = "(()(()))";
+        char openCharacter = '(';
+        char closeCharacter = ')';
+        
+        int depthLevel = 0;
+        int maxDepthLevel = depthLevel;
+        
+        foreach (char element in expression)
+        {
+            if (element == openCharacter)
+                depthLevel++;
+            else if (element == closeCharacter)
+                depthLevel--;
+            
+            if (depthLevel < 0)
+                break;
+            
+            if (depthLevel > maxDepthLevel)
+                maxDepthLevel = depthLevel;
+        }
+        
+        if (depthLevel == 0)
+        {
+            Console.WriteLine($"Выражение \"{expression}\" является корректным");
+            Console.WriteLine($"Максимальный уровень вложенности: {maxDepthLevel}");
+        }
+        else
+        {
+            Console.WriteLine($"Выражение \"{expression}\" не является корректным");
+        }
+    }
+}
