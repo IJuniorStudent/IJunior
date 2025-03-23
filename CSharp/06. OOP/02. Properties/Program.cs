f4aa59c1a633f41e48c4b379e01c4cd4b5f2caa1
IJuniorStudent <jolan07@list.ru>
2025-03-24 02:40:14 +0300

    Practice 38 attempt 1

---
+++ CSharp/06. OOP/02. Properties/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Practice_38;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int playerStartX = 10;
+        int playerStartY = 10;
+        char playerAppearance = '@';
+        ConsoleKey exitKey = ConsoleKey.E;
+        
+        Console.CursorVisible = false;
+        
+        Player player = new Player(playerStartX, playerStartY, playerAppearance);
+        bool isAppRun = true;
+        
+        while (isAppRun)
+        {
+            Console.Clear();
+            
+            Rederer.DrawPlayer(player);
+            
+            ConsoleKey pressedKey = Console.ReadKey(true).Key;
+            
+            if (pressedKey == exitKey)
+                isAppRun = false;
+        }
+    }
+}
+
+class Player
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+    public char Appearance { get; private set; }
+    
+    public Player(int x, int y, char appearance)
+    {
+        X = x;
+        Y = y;
+        Appearance = appearance;
+    }
+}
+
+class Rederer
+{
+    public static void DrawPlayer(Player player)
+    {
+        Console.SetCursorPosition(player.X, player.Y);
+        Console.Write(player.Appearance);
+    }
+}
