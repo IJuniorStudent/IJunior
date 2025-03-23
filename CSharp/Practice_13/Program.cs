eb79735ddb1ded82d4233e9a7462e70ea4a4caa1
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:42:09 +0300

    Practice 13 attempt 1

---
+++ CSharp/Practice_13/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Practice_13;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int BoxBorderWidth = 1;
+        
+        Console.Write("Введите ваше имя: ");
+        string userName = Console.ReadLine();
+        
+        Console.Write("Введите символ, котороым будет нарисована рамка: ");
+        char borderChar = Console.ReadLine()[0];
+        
+        int userNameLength = userName.Length;
+        
+        for (int i = 0; i < BoxBorderWidth; i++)
+        {
+            for (int j = 0; j < userNameLength + BoxBorderWidth + BoxBorderWidth; j++)
+                Console.Write(borderChar);
+            
+            Console.WriteLine();
+        }
+        
+        for (int i = 0; i < BoxBorderWidth; i++)
+            Console.Write(borderChar);
+        
+        Console.Write(userName);
+        
+        for (int i = 0; i < BoxBorderWidth; i++)
+            Console.Write(borderChar);
+        
+        Console.WriteLine();
+        
+        for (int i = 0; i < BoxBorderWidth; i++)
+        {
+            for (int j = 0; j < userNameLength + BoxBorderWidth + BoxBorderWidth; j++)
+                Console.Write(borderChar);
+            
+            Console.WriteLine();
+        }
+    }
+}
