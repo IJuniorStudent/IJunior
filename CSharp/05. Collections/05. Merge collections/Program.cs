9b2e769d1ac1e59694acb6642cb22c7479052cf9
IJuniorStudent <jolan07@list.ru>
2025-03-24 02:36:44 +0300

    Practice 36 attempt 1

---
+++ CSharp/05. Collections/05. Merge collections/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Practice_36;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> firstList = ["1", "2", "1"];
+        List<string> secondList = ["3", "2"];
+        
+        List<string> uniqueValueList = MergeUniqueValues([firstList, secondList]);
+        
+        PrintList(uniqueValueList);
+    }
+    
+    static List<string> MergeUniqueValues(List<List<string>> sourceLists)
+    {
+        var mergedList = new List<string>();
+        
+        foreach (var sourceList in sourceLists)
+        {
+            foreach (var value in sourceList)
+            {
+                if (mergedList.Contains(value))
+                    continue;
+                
+                mergedList.Add(value);
+            }
+        }
+        
+        return mergedList;
+    }
+    
+    static void PrintList(List<string> list)
+    {
+        foreach (var value in list)
+            Console.Write($"{value} ");
+    }
+}
