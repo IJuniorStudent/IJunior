23807534195e669e1cee76a4eabe1f8f49076af9
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:43:54 +0300

    Practice 14 attempt 1

---
+++ CSharp/Practice_14/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Practice_14;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string unlockPassword = "secrets";
+        int inputPasswordAttemptsRemain = 3;
+ 
+        while (--inputPasswordAttemptsRemain >= 0)
+        {
+            Console.Write("Введите пароль для разблокировки: ");
+            if (Console.ReadLine() == unlockPassword)
+                break;
+            
+            Console.WriteLine("Неверный пароль");
+        }
+ 
+        if (inputPasswordAttemptsRemain >= 0)
+            Console.WriteLine("Секретики!");
+    }
+}
