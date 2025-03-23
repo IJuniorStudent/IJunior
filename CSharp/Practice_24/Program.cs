94816d2b748cefd603225c22d791ecb561dfc776
IJuniorStudent <jolan07@list.ru>
2025-03-24 01:02:23 +0300

    Practice 24 attempt 1

---
+++ CSharp/Practice_24/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Practice_24;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sourceMessage = "Lorem ipsum dolor sit amet";
+        
+        string[] messageWords = sourceMessage.Split();
+        
+        foreach (string word in messageWords)
+            Console.WriteLine(word);
+    }
+}
