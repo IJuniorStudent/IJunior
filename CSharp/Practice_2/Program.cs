42ed9db868671d377fe79099cf436147d3dcc796
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:10:10 +0300

    Practice 2 fix

--- CSharp/Practice_2/Program.cs
+++ CSharp/Practice_2/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace Practice_2;
+﻿using System.Text;
+
+namespace Practice_2;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.InputEncoding = Encoding.Unicode;
+        Console.OutputEncoding = Encoding.Unicode;
+        
+        Console.Write("Как вас зовут: ");
+        string userName = Console.ReadLine();
+        
+        Console.Write("Сколько вам лет: ");
+        int userAge = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Кто вы по знаку зодиака: ");
+        string zodiacName = Console.ReadLine();
+        
+        Console.Write("Где вы работаете: ");
+        string workName = Console.ReadLine();
+        
+        Console.WriteLine($"Вас зовут {userName}, вам {userAge}, вы {zodiacName} и работаете {workName}.");
+        Console.ReadKey();
     }
 }
