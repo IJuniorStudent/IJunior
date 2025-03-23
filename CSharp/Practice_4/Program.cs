6baa2321ef8480712dbf088acf97c1cb653def5a
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:12:34 +0300

    Practice 4 attempt 1

---
+++ CSharp/Practice_4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Practice_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string firstName = "Пупкин";
+        string lastName = "Василий";
+        
+        string leftCup = "Кофе";
+        string rightCup = "Чай";
+        
+        Console.WriteLine($"Данные до перестановки. Имя: {firstName}, Фамилия: {lastName}");
+        Console.WriteLine($"Содержимое до перестановки. Левая чашка: {leftCup}, правая чашка: {rightCup}");
+        Console.WriteLine();
+        
+        (firstName, lastName) = (lastName, firstName);
+        (leftCup, rightCup) = (rightCup, leftCup);
+ 
+        Console.WriteLine($"Данные после перестановки. Имя: {firstName}, Фамилия: {lastName}");
+        Console.WriteLine($"Содержимое после перестановки. Левая чашка: {leftCup}, правая чашка: {rightCup}");
+    }
+}
