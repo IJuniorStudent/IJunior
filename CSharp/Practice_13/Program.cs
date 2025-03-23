c773b5cf900430cc051cdddb0f56651523d1e2ac
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:42:59 +0300

    Practice 13 attempt 2

--- CSharp/Practice_13/Program.cs
+++ CSharp/Practice_13/Program.cs
@@ -4,40 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        const int BoxBorderWidth = 1;
-        
         Console.Write("Введите ваше имя: ");
         string userName = Console.ReadLine();
         
         Console.Write("Введите символ, котороым будет нарисована рамка: ");
         char borderChar = Console.ReadLine()[0];
         
-        int userNameLength = userName.Length;
+        string userNameBordered = $"{borderChar}{userName}{borderChar}";
+        string border = "";
         
-        for (int i = 0; i < BoxBorderWidth; i++)
-        {
-            for (int j = 0; j < userNameLength + BoxBorderWidth + BoxBorderWidth; j++)
-                Console.Write(borderChar);
-            
-            Console.WriteLine();
-        }
+        for (int i = 0; i < userNameBordered.Length; i++)
+            border += borderChar;
         
-        for (int i = 0; i < BoxBorderWidth; i++)
-            Console.Write(borderChar);
-        
-        Console.Write(userName);
-        
-        for (int i = 0; i < BoxBorderWidth; i++)
-            Console.Write(borderChar);
-        
-        Console.WriteLine();
-        
-        for (int i = 0; i < BoxBorderWidth; i++)
-        {
-            for (int j = 0; j < userNameLength + BoxBorderWidth + BoxBorderWidth; j++)
-                Console.Write(borderChar);
-            
-            Console.WriteLine();
-        }
+        Console.WriteLine(border);
+        Console.WriteLine(userNameBordered);
+        Console.WriteLine(border);
     }
 }
