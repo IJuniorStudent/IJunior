7f58a9b5486e5cde2625ed2beea329991797f33d
IJuniorStudent <jolan07@list.ru>
2025-03-24 02:38:58 +0300

    Practice 37 attempt 1

---
+++ CSharp/06. OOP/01. Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Practice_37;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player[] players =
+        [
+            new Player("Дмитрий", 100),
+            new Player("Игорь", 9000),
+            new Player("Максим", 359)
+        ];
+        
+        foreach (var player in players)
+            player.ShowInfo();
+    }
+}
+
+class Player
+{
+    private string _name;
+    private int _score;
+    
+    public Player(string name, int score)
+    {
+        _name = name;
+        _score = score;
+    }
+    
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Меня зовут: {_name}, мой счет: {_score} очков");
+    }
+}
