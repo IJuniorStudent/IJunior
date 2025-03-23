dc766756014da59e058bf67bb7a8d5737c848623
IJuniorStudent <jolan07@list.ru>
2025-03-24 00:13:33 +0300

    Practice 5 attempt 1

---
+++ CSharp/Practice_5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Practice_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int gemPrice = 10;
+        
+        Console.Write("Введите количество вашего золота: ");
+        int goldAmount = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Напишите, сколько кристаллов вы хотите купить: ");
+        int gemCount = Convert.ToInt32(Console.ReadLine());
+        
+        goldAmount -= gemCount * gemPrice;
+        
+        Console.WriteLine($"Куплено кристаллов: {gemCount}");
+        Console.WriteLine($"Осталось золота: {goldAmount}");
+        Console.ReadKey();
+    }
+}
