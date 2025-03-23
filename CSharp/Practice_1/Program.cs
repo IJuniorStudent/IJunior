c517fff705c5d26281b63784b6249e0dbda4efef
IJuniorStudent <jolan07@list.ru>
2025-03-23 23:55:24 +0300

    Practice 1 attempt 1

---
+++ CSharp/Practice_1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Practice_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte byteValueUnit, byteValueZero;
+        byteValueUnit = 1;
+        byteValueZero = 0;
+ 
+        ushort unsignedShortValueMax;
+        unsignedShortValueMax = ushort.MaxValue;
+ 
+        short shortValueNegative;
+        shortValueNegative = -359;
+ 
+        float floatValueSinglePrecision;
+        floatValueSinglePrecision = 0.123456f;
+ 
+        double floatValueDoublePrecision;
+        floatValueDoublePrecision = 0.123456789;
+ 
+        char singleCharValue;
+        singleCharValue = 'X';
+ 
+        string stringValue;
+        stringValue = "here is a string. Hello!";
+ 
+        bool boolValue;
+        boolValue = true;
+ 
+        bool isUnitLessThanZero = byteValueUnit < byteValueZero;
+    }
+}
